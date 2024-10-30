a493448d572ad11d43917624a3b6fe598d86f119
Hugo-Jasmin <hugomyuser@gmail.com>
2024-10-30 10:28:55 +0700

    Draft 1 of NGC3

--- testing.py
+++ testing.py
@@ -1,14 +1,43 @@
-def Fibbonaci(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    fibonaci_list = [0,1]
-    x = 2 
-    while x < n:
-        numberAdded = fibonaci_list[x-2]+fibonaci_list[x-1]
-        fibonaci_list.append(numberAdded)
-        x +=1
-    print(fibonaci_list)
-    return(fibonaci_list[n-1])
-print(Fibbonaci(10))
+#import random
+import os
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+from PIL import Image, ImageTk
+import tkinter as tk
+window = tk.Tk()
+window.geometry("800x600")
+
+
+booksAvailable = []
+class book:
+    def __init____init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+    # def creation(self):
+
+def bookCreationScreen():
+    creationPage = tk.Frame(window)
+    mainpage.pack_forget()  
+    creationPage.pack()
+    window.title("Create Book")
+    window.geometry("400x300")
+    titleInputTopText = tk.Label(creationPage, text= "please set a title for the work")
+    titleInputTopText.grid(row= 1, column= 0)
+    titleInput = tk.Entry(creationPage, width=40)
+    titleInput.grid(row= 2, column= 0)
+    authorInputTopText = tk.Label(creationPage, text= "please set an author for this work")
+    authorInputTopText.grid(row= 3, column= 0)
+    titleInput = tk.Entry(creationPage, width=40)
+    titleInput.grid(row= 4, column= 0)
+def mainpage():
+    global mainpage
+    mainpage = tk.Frame(window)
+    mainpage.pack()
+    window.title("main menu")
+    button1 = tk.Button(mainpage, text='Create', width=50, command= bookCreationScreen)
+    button1.grid(row=1, column=0)
+
+mainpage()
+window.mainloop()
